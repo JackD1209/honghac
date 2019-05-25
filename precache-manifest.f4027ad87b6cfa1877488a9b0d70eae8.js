9b3a5e195f7d3482db558d7f52a9b33f64562343
HoangDoan <hoang@zappasoft.com>
2019-05-25 18:47:07 +0700

    Updates

---
+++ precache-manifest.f4027ad87b6cfa1877488a9b0d70eae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fa8c7487bf725ff1f954c8cbd5001b",
+    "url": "/JackD1209/honghac/index.html"
+  },
+  {
+    "revision": "fb18f18085a39302c39e",
+    "url": "/JackD1209/honghac/static/css/main.5b9d06cf.chunk.css"
+  },
+  {
+    "revision": "94af7f8b07d7d4877212",
+    "url": "/JackD1209/honghac/static/js/2.15fbe00d.chunk.js"
+  },
+  {
+    "revision": "fb18f18085a39302c39e",
+    "url": "/JackD1209/honghac/static/js/main.8f0a10af.chunk.js"
+  },
+  {
+    "revision": "c04108e6d8fc4c0b6e15",
+    "url": "/JackD1209/honghac/static/js/runtime~main.8e93d859.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JackD1209/honghac/static/media/logo.5d5d9eef.svg"
+  }
+]);
